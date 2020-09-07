1d70abae694f37a7baf74a3fa0fbec7e66a2b4c3
zerrora <lawdarylr@gmail.com>
2020-09-07 22:56:40 +0800

    updated

---
+++ unit test code.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+
+class testmyprogram(unittest.TestCase):
+
+    def test(self):
+        print("Testing")
+        os.system("C:\\Users\\jackripper\\Documents\\GITHUB repo\\.vscode\\hello-world\\proj.py")
+
+if __name__ == "__main__":
+    unittest.main()
+
+process = unittest
